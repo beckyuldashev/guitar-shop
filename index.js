089ef7a023b8e514512729cb69fd968fe8d9dab4
Beck Yuldashev <yuldashev.bek@gmail.com>
2023-01-28 19:46:35 +0300

    Реализация сервера через firebase

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-(() => {
+let CATALOG = [];
+const url = 'https://sneakers-145dd-default-rtdb.firebaseio.com/catalog.json';
+
+function render() {
   const productsStore = localStorageUtil.getProducts();
   productsPage.render();
   headerPage.render(productsStore.length);
-})();
+}
+
+function getData(url) {
+  return fetch(url).then(res => res.json());
+} 
+
+getData(url)
+  .then(data => {
+    CATALOG = data;
+    render();
+  })
+  .catch(err => {
+    console.log(err);
+  });
